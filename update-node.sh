6bc44812f8252c958d5f034452b8eb44cde5692c
Gian Felipe <gianfelipe@Gians-MacBook-Pro-2.local>
2018-06-18 12:39:24 +1000

    Chaging yaml file to automate deployment

--- update-node.sh
+++ update-node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ssh -oStrictHostKeyChecking=no gian@191.237.250.16
+ssh -oStrictHostKeyChecking=no blockfreight-travis@191.237.250.16
 rm app.yaml && curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml
 kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force
