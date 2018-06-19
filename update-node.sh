6e886d19b4e355cb92171ace49f81519588d22dd
Gian Felipe <gianfelipe@Gians-MacBook-Pro-2.local>
2018-06-19 13:09:58 +1000

    Chaging yaml file to automate deployment

--- update-node.sh
+++ update-node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 bftx0=$BFTX0_MASTER_IP
-echo bftx0
+echo "$bftx0"
 
 ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$BFTX0_MASTER_IP  <<-'ENDSSH'
     curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml
