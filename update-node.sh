201e7cfe2d9b66d2b378ff0219be79a73d552173
Gian Felipe <gianfelipe@Gians-MacBook-Pro-2.local>
2018-06-19 12:15:06 +1000

    Chaging yaml file to automate deployment

--- update-node.sh
+++ update-node.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+ssh -oStrictHostKeyChecking=no blockfreight-travis@104.210.61.36  <<-'ENDSSH'
+    curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml
+    kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force
+    rm app.yaml
+ENDSSH
+
 ssh -oStrictHostKeyChecking=no blockfreight-travis@191.237.250.16  <<-'ENDSSH'
-    pwd
+    curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml
+    kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force
+    rm app.yaml
+ENDSSH
+
+ssh -oStrictHostKeyChecking=no blockfreight-travis@104.45.28.199  <<-'ENDSSH'
+    curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml
+    kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force
+    rm app.yaml
+ENDSSH
+
+ssh -oStrictHostKeyChecking=no blockfreight-travis@13.78.36.220  <<-'ENDSSH'
     curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml
     kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force
     rm app.yaml
