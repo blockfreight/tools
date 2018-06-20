398c4c3e68e8ffff9cbe4e4746100dbf20baf98f
Gian Felipe <gianfelipe@Gians-MacBook-Pro-2.local>
2018-06-20 12:43:20 +1000

    Chaging yaml file to automate deployment

--- update-node.sh
+++ update-node.sh
@@ -9,7 +9,7 @@ do
 
     ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator  <<-'ENDSSH'
     curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml
-    sed -i -- 's/<VALIDATOR_NAME>/$validator_name/g' *
+    sed -i -- 's/<VALIDATOR_NAME>/'$validator_name'/g' *
     cat app.yaml
     kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force
     rm app.yaml
