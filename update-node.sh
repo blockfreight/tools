4af26941eb63c100cbc1ae4aea0afbcfcf9f889c
Gian Felipe <gianfelipe@Gians-MacBook-Pro-2.local>
2018-06-20 13:01:52 +1000

    Chaging yaml file to automate deployment

--- update-node.sh
+++ update-node.sh
@@ -5,14 +5,8 @@ for validator in $BFTX0_MASTER_IP $BFTX1_MASTER_IP $BFTX2_MASTER_IP $BFTX3_MASTE
 do
     validator_name=bftx${index}
 
-    echo $validator_name
-
-    ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator  <<-'ENDSSH'
-    curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml
-    sed -i -- 's/<VALIDATOR_NAME>/'$validator_name'/g' *
-    cat app.yaml
-    kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force
-    rm app.yaml
+    ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;echo $validator_name;curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;sed -i -- 's/<VALIDATOR_NAME>/'$validator_name'/g' *;cat app.yaml;kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force" <<-'ENDSSH'
+    
 ENDSSH
     ((index+=1))
 done
