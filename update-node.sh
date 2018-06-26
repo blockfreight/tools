8bdad63209daa38426ebc7b2c45d721c21fef394
Gian Felipe <gianfelipe@Gians-MacBook-Pro-2.local>
2018-06-26 13:02:52 +1000

    Added test environment variables to Travis

--- update-node.sh
+++ update-node.sh
@@ -6,6 +6,8 @@ do
     validator_name=bftx${index}
     private_type=${PRIVATE_KEY_TYPE}
     private_key=${PRIVATE_KEY_BFTX+index}
+    echo $private_type;
+    echo $private_key;
 
    ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;echo $private_type;echo $private_key;curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;sed -i -- 's/<VALIDATOR_NAME>/'$validator_name'/g' *;sed -i -- 's/<PRIVATE_KEY>/'$private_key'/g' *;sed -i -- 's/<PRIVATE_TYPE>/'$private_type'/g' *;cat app.yaml; rm app.yaml;" <<-'ENDSSH'
     
