1390f03805b544ae11e43c61008dea5b7c38f337
Gian Felipe <gianfelipe@Gians-MacBook-Pro-2.local>
2018-06-26 12:45:34 +1000

    Chaging yaml file to automate deployment

--- update-node.sh
+++ update-node.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 index=0
-for validator in $BFTX0_MASTER_IP $BFTX1_MASTER_IP $BFTX2_MASTER_IP $BFTX3_MASTER_IP
+for validator in $BFTX0_MASTER_IP # $BFTX1_MASTER_IP $BFTX2_MASTER_IP $BFTX3_MASTER_IP
 do
     validator_name=bftx${index}
+    private_type=${PRIVATE_KEY_TYPE}
+    private_type=${PRIVATE_KEY_BFTX${index}}
 
-    # ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;echo $validator_name;curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;sed -i -- 's/<VALIDATOR_NAME>/'$validator_name'/g' *;kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force" <<-'ENDSSH'
+
+   ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;echo $validator_name;curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;sed -i -- 's/<VALIDATOR_NAME>/'$validator_name'/g' *;sed -i -- 's/<PRIVATE-KEY>/'$private_key'/g' *;sed -i -- 's/<PRIVATE-TYPE>/'$private_type'/g' *;cat app.yaml; rm app.yaml;" <<-'ENDSSH'
     
-    curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml
-    sed -i -- 's/<VALIDATOR_NAME>/'$validator_name'/g' *
-    cat app.yaml
 ENDSSH
     ((index+=1))
 done
