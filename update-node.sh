76c1eced8979722ff60968262ac1c77474d5e457
Gian Felipe <gianfelipe@Gians-MacBook-Pro-2.local>
2018-06-27 12:51:46 +1000

    removing old yaml files

--- update-node.sh
+++ update-node.sh
@@ -11,7 +11,7 @@ do
     private_node_key=${private_node_keys[index]}
 
    ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;rm app.yaml;curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;sed -i -- 's/<VALIDATOR_NAME>/'$validator_name'/g' app.yaml;sed -i -- 's/<PRIVATE_KEY>/'$private_key'/g' app.yaml;sed -i -- 's/<PRIVATE_TYPE>/'$private_type'/g' app.yaml;sed -i -- 's/<PRIVATE_NODE_KEY>/'$private_node_key'/g' app.yaml;ls;cat app.yaml;kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force;rm app2.yaml pre_app.yaml" <<-'ENDSSH'
-    
+
 ENDSSH
     ((index+=1))
 done
