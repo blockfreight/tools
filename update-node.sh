1b67c5e0eb77f5c2b897cca135e3a431c5f2498a
Gian Felipe <gianfelipe@Gians-MacBook-Pro-2.local>
2018-06-27 13:14:10 +1000

    removing old yaml files

--- update-node.sh
+++ update-node.sh
@@ -3,14 +3,14 @@
 index=0
 private_keys=( $PRIVATE_KEY_BFTX0 $PRIVATE_KEY_BFTX1 $PRIVATE_KEY_BFTX2 $PRIVATE_KEY_BFTX3 )
 private_node_keys=( $PRIVATE_NODE_KEY_BFTX0 $PRIVATE_NODE_KEY_BFTX1 $PRIVATE_NODE_KEY_BFTX2 $PRIVATE_NODE_KEY_BFTX3 )
-for validator in $BFTX0_MASTER_IP $BFTX1_MASTER_IP $BFTX2_MASTER_IP $BFTX3_MASTER_IP
+for validator in $BFTX0_MASTER_IP #$BFTX1_MASTER_IP $BFTX2_MASTER_IP $BFTX3_MASTER_IP
 do
     validator_name=bftx${index}
     private_type=${PRIVATE_KEY_TYPE}
     private_key=${private_keys[index]}
     private_node_key=${private_node_keys[index]}
 
-   ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;rm app.yaml;curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;sed -i -- 's/<VALIDATOR_NAME>/$validator_name/g' app.yaml;sed -i -- 's/<PRIVATE_KEY>/$private_key/g' app.yaml;sed -i -- 's/<PRIVATE_TYPE>/$private_type/g' app.yaml;sed -i -- 's/<PRIVATENODEKEY>/$private_node_key/g' app.yaml;ls;cat app.yaml;kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force;rm app2.yaml pre_app.yaml" <<-'ENDSSH'
+   ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;rm app.yaml;curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;sed -i -- 's/<VALIDATOR_NAME>/$validator_name/g' app.yaml;sed -i -- 's/<PRIVATE_KEY>/$private_key/g' app.yaml;sed -i -- 's/<PRIVATE_TYPE>/$private_type/g' app.yaml;sed -i -- 's/<PRIVATENODEKEY>/test/g' app.yaml;ls;cat app.yaml;kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force;rm app2.yaml pre_app.yaml" <<-'ENDSSH'
 
 ENDSSH
     ((index+=1))
