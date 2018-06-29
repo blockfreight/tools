bb9cb4ba71b368519625767af3d0085b0575c922
Gian Felipe <gianfelipe@Gians-MacBook-Pro-2.local>
2018-06-29 10:23:16 +1000

    TRying to replace the keys for the last time

--- update-node.sh
+++ update-node.sh
@@ -8,9 +8,16 @@ do
     validator_name=bftx${index}
     private_type=${PRIVATE_KEY_TYPE}
     private_key=${private_keys[index]}
-    private_node_key=${private_node_keys[2]}
+    private_node_key=${private_node_keys[index]}
 
-   ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;rm app.yaml;curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;sed -i -- 's/<VALIDATOR_NAME>/$validator_name/g' app.yaml;sed -i -- 's/<PRIVATE_KEY>/$private_key/g' app.yaml;sed -i -- 's/<PRIVATE_TYPE>/$private_type/g' app.yaml;cat app.yaml;kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force;rm app2.yaml pre_app.yaml" <<-'ENDSSH'
+   ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;\
+   rm app.yaml;curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;\
+   sed -i -- 's/<VALIDATOR_NAME>/$validator_name/g' app.yaml;\
+   sed -i -- 's/<PRIVATE_KEY>/$private_key/g' app.yaml;\
+   sed -i -- 's/<PRIVATE_TYPE>/$private_type/g' app.yaml;\
+   sed -i -- 's/<PRIVATE_NODE_KEY>/$private_node_key/g' app.yaml;\
+   cat app.yaml;\
+   kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force;" <<-'ENDSSH'
 
 ENDSSH
     ((index+=1))
