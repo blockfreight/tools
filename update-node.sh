3750ddcf46b0f3c32085621b8bcc08138d0aa933
Gian Felipe <gianfelipe@Gians-MacBook-Pro-2.local>
2018-06-29 11:23:26 +1000

    Started using kubernetes secrets

--- update-node.sh
+++ update-node.sh
@@ -11,11 +11,14 @@ do
     private_node_key=${private_node_keys[index]}
 
    ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;\
+    env private_type=$private_type;\
+    env private_key=$private_key;\
+    env private_node_key=$private_node_key;\
+
+    kubectl create secret generic node_private_keys --from-literal=$private_key --from-literal=$private_type --from-literal=$private_node_key
+
    rm app.yaml;curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;\
    sed -i -- 's/<VALIDATOR_NAME>/$validator_name/g' app.yaml;\
-   sed -i -- 's/<PRIVATE_KEY>/$private_key/g' app.yaml;\
-   sed -i -- 's/<PRIVATE_TYPE>/$private_type/g' app.yaml;\
-   sed -i -- 's/<PRIVATE_NODE_KEY>/$private_node_key/g' app.yaml;\
    cat app.yaml;\
    kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force;" <<-'ENDSSH'
 
