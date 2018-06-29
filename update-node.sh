77bc879b3f79991556186fbe7eb894b9c663a6c7
Gian Felipe <gianfelipe@Gians-MacBook-Pro-2.local>
2018-06-29 12:48:14 +1000

    Started using kubernetes secrets

--- update-node.sh
+++ update-node.sh
@@ -16,6 +16,7 @@ do
     env private_node_key=$private_node_key;\
     
     kubectl create secret generic node_private_keys --from-literal=$private_key --from-literal=$private_type --from-literal=$private_node_key;\
+    ls;\
     rm app.yaml;\
     curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;\
     sed -i -- 's/<VALIDATOR_NAME>/$validator_name/g' app.yaml;\
