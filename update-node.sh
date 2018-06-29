2c6fd32b9b8c6b5e6ea775948ae251a976512a94
Gian Felipe <gianfelipe@Gians-MacBook-Pro-2.local>
2018-06-29 12:36:51 +1000

    Started using kubernetes secrets

--- update-node.sh
+++ update-node.sh
@@ -3,10 +3,10 @@
 index=0
 private_keys=( $PRIVATE_KEY_BFTX0 $PRIVATE_KEY_BFTX1 $PRIVATE_KEY_BFTX2 $PRIVATE_KEY_BFTX3 )
 private_node_keys=( $PRIVATE_NODE_KEY_BFTX0 $PRIVATE_NODE_KEY_BFTX1 $PRIVATE_NODE_KEY_BFTX2 $PRIVATE_NODE_KEY_BFTX3 )
+
 for validator in $BFTX0_MASTER_IP $BFTX1_MASTER_IP $BFTX2_MASTER_IP $BFTX3_MASTER_IP
 do
     validator_name=bftx${index}
-    private_type=$PRIVATE_KEY_TYPE
     private_key=${private_keys[index]}
     private_node_key=${private_node_keys[index]}
 
@@ -14,13 +14,16 @@ do
     env private_type=$private_type;\
     env private_key=$private_key;\
     env private_node_key=$private_node_key;\
-
+    echo $private_node_key
+    echo $private_key
+    echo $private_type
+    
     kubectl create secret generic node_private_keys --from-literal=$private_key --from-literal=$private_type --from-literal=$private_node_key;\
-
-   rm app.yaml;curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;\
-   sed -i -- 's/<VALIDATOR_NAME>/$validator_name/g' app.yaml;\
-   cat app.yaml;\
-   kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force;" <<-'ENDSSH'
+    rm app.yaml;\
+    curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;\
+    sed -i -- 's/<VALIDATOR_NAME>/$validator_name/g' app.yaml;\
+    cat app.yaml;\
+    kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force;" <<-'ENDSSH'
 
 ENDSSH
     ((index+=1))
